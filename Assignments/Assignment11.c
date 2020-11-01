0565a69a01f5ffcf065d3f50796628e6e5b1675d
Menna <menna_nasser28@yahoo.com>
2020-11-01 20:54:18 +0200

    Creating Assignment11.c and generating result11.exe

--- Assignments/Assignment11.c
+++ Assignments/Assignment11.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(){
+	
 	int num1, num2;
 	printf("Enter the first integer number: ");
 	scanf("%d", &num1);
 	printf("Enter the second integer number: ");
 	scanf("%d", &num2);
-	printf("Result of the equation is: %d", ((num2+num2)*3)-10 );
+	printf("Result of the equation is: %d", (((num1+num2)*3)-10) );
+	
 }
