4b3fbf384b217651350226dbb109ff52268780e2
Menna <menna_nasser28@yahoo.com>
2020-11-01 20:49:57 +0200

    Adding Assignments directory

---
+++ Assignments/Assignment11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int num1, num2;
+	printf("Enter the first integer number: ");
+	scanf("%d", &num1);
+	printf("Enter the second integer number: ");
+	scanf("%d", &num2);
+	printf("Result of the equation is: %d", ((num2+num2)*3)-10 );
+}
