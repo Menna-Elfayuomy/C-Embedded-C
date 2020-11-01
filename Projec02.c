d2f50e333f5802e8ea62b5d871c6830126d7c6bb
Menna <menna_nasser28@yahoo.com>
2020-11-01 11:42:30 +0200

    Adding Project02.c printing out a bio - generating executable file Out02.exe file

---
+++ Projec02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	printf("I'm Menna Nasser \n");
+	printf("My birth date is 28 Nov 1996 \n");
+	printf("I graduated from faculty of engineering \n");
+	printf ("Ain-Shams University");	
+}
--- Project01.c
+++ Project01.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include <stdio.h>		//This is the standard Input Output library
 
 int main()
 {
+	/* this is the main function
+		this is the entry point of the program */
 	printf("Hello World");
 	return 0;
 }
