0acfe6255aa88121de048b45889fd9df2c1cf4c6
Menna <menna_nasser28@yahoo.com>
2020-11-01 21:08:07 +0200

    Adding Assignment13.c file and generating result13.exe
    Program used to convert temperature from Celsius to Fahrenheit

---
+++ Assignments/Assignment13.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	float Celsius, Fahrenheit;
+	printf("Please enter the temperature in Celsius: ");
+	scanf("%f", &Celsius);
+	Fahrenheit = Celsius*(9/5)+32;
+	printf("Temperature in Fahrenheit: %f", Fahrenheit);
+}
