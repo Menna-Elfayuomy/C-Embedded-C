6c6cb5dc68ed2721f05f8e5305f8fc9818bf4d6f
Menna <menna_nasser28@yahoo.com>
2020-11-01 14:28:30 +0200

    Adding Project04.c file and generating out4.exe
    Project04.c program to take 2 numbers from the user and apply different
    arithmetic operations on them as following: sum - multiplication -
    division.
    And for division we consider the probability of having float result.

---
+++ Project04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int a, b;
+	printf("Please Enter number 1: ");
+	scanf("%d", &a);
+	printf("Please Enter number 2: ");
+	scanf("%d", &b);
+	printf("Sum = %d \n", a+b);
+	printf("Mul. = %d \n", a*b);
+	printf ("Div. = %f", (float)a/b);
+}
