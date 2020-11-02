043a7d34abb835c0fbf7d460ef17bbc586f43579
Menna-hdd <menna_nasser28@yahoo.com>
2020-11-02 14:22:53 +0200

    Adding program to check whose ID is it. using switch case

---
+++ Labs/02-Labs/09-Lab.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int x;
+	printf("Please Enter Your ID: ");
+	scanf("%d", &x);
+	switch (x){
+		case 1234:
+		printf("Welcome Ahmed");
+		break;
+		case 5678:
+		printf("Welcome Youssef");
+		break;
+		case 1145:
+		printf("Welcome Mina");
+		break;
+		default:
+		printf("Invalid ID! You're a hacker -__-");
+		break;
+	}
+}
