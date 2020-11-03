8d0569819052469b22b11901742db6f4d0cf2fa2
Menna <menna_nasser28@yahoo.com>
2020-11-03 19:33:04 +0200

    Adding 02-pb.c and 02-solu.exe to print the alphabet in uppercase and lowercase

---
+++ Assignments/03-Assignment/02-pb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	for (int i='A'; i<='Z'; i++){
+		printf("%c ", i);
+	}
+	printf("\n");
+	for (int i='a'; i<='z'; i++){
+		printf("%c ", i);
+	}
+	
+	return 0;
+}
