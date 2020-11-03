e53c0553e5b15cba3e697fe90b02934ce22185fd
Menna <menna_nasser28@yahoo.com>
2020-11-03 12:53:19 +0200

    Updating Lab 4 to get 10 numbers from the user and get their sum and average

--- Labs/03-Labs/04-Lab.c
+++ Labs/03-Labs/04-Lab.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 int main(){
+	int num;
 	int sum=0;
 	float av;
+	
 	// loop to print my name 10 times
 	for (int i=0; i<10; i++){
 		printf("Menna\n");
@@ -11,7 +13,13 @@ int main(){
 	// loop to print first 10 natural numbers
 	for (int x=1; x<=10; x++){
 		printf("%d ", x);
-		sum = sum +x;
+	}
+	
+	//loop to read 10 numbers from the user to get their sum and average
+	for (int y=1; y<11; y++){
+		printf("\n Number %d: ", y);
+		scanf("%d", &num);
+		sum = sum+num;
 	}
 	av = (float)sum/10;
 	printf("\nSum of 10 numbers is: %d\n", sum);
