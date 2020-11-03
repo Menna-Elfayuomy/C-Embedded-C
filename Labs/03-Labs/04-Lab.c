462820423757ee5914cfbb29f258e636209ac6d9
Menna <menna_nasser28@yahoo.com>
2020-11-03 12:12:01 +0200

    Updating Lab4 to get the sum and average of the ten natural number being printed

--- Labs/03-Labs/04-Lab.c
+++ Labs/03-Labs/04-Lab.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(){
+	int sum=0;
+	float av;
 	// loop to print my name 10 times
 	for (int i=0; i<10; i++){
 		printf("Menna\n");
@@ -9,6 +11,11 @@ int main(){
 	// loop to print first 10 natural numbers
 	for (int x=1; x<=10; x++){
 		printf("%d ", x);
+		sum = sum +x;
 	}
+	av = (float)sum/10;
+	printf("\nSum of 10 numbers is: %d\n", sum);
+	printf("Average of 10 numbers is: %f\n", av);
+	
 	return 0;
 }
