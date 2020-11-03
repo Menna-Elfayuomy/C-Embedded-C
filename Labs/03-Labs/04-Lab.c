c9e6524628cd0bbce9a3c275264698d8e9135f17
Menna <menna_nasser28@yahoo.com>
2020-11-03 11:59:14 +0200

    Updating Lab 4 to print out the first natural 10 numbers

--- Labs/03-Labs/04-Lab.c
+++ Labs/03-Labs/04-Lab.c
@@ -2,8 +2,13 @@
 
 int main(){
 	// loop to print my name 10 times
-	for (int i=0; i<=10; i++){
+	for (int i=0; i<10; i++){
 		printf("Menna\n");
+	}
+	
+	// loop to print first 10 natural numbers
+	for (int x=1; x<=10; x++){
+		printf("%d ", x);
 	}
 	return 0;
 }
