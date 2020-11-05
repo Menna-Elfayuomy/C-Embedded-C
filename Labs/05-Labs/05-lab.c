0577936257f9a9bc13473ccd955ec8f9c6674da3
Menna <menna_nasser28@yahoo.com>
2020-11-05 14:51:28 +0200

    Changing for loop that prints the values to print it in reversed order and changing the please enter number i value to be user friendly

--- Labs/05-Labs/05-lab.c
+++ Labs/05-Labs/05-lab.c
@@ -2,14 +2,12 @@
 
 int main(){
 	int arr[10];
-	int element;
 	for (int i=0; i<10; i++){
-		printf("Please enter number %d: ", i);
+		printf("Please enter number %d: ", (i+1));
 		scanf("%d", &arr[i]);
-//		arr[i]=element;
 	}
 	printf("The values in reversed order\n");
-	for (int i=0; i<10; i++){
+	for (int i=9; i>0; i--){
 		printf("%d",arr[i]);
 		printf("\n");
 	}
