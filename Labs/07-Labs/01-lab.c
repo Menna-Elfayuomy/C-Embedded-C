6d543a2c14178b176b7b2f25d35a80a47a26a72f
Menna <menna_nasser28@yahoo.com>
2020-11-09 10:07:22 +0200

    Adding lab 1 files - takes a string array using gets function and return (print out) the array after removing any spaces in the string

--- Labs/07-Labs/01-lab.c
+++ Labs/07-Labs/01-lab.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+#include <conio.h>
+#define SIZE 50
 
+int main(){
+	char arr[SIZE], new_arr[SIZE];
+	int i=0, j=0;
+	
+	printf("Enter a string: ");
+	gets(arr);
+	
+	while (arr[i]!='\0'){
+		if (arr[i] != ' '){
+			new_arr[j]=arr[i];
+			j++;
+		}
+		i++;
+	}
+	
+	printf("You entered %s: ", new_arr);
+	
+	return 0;
+}
