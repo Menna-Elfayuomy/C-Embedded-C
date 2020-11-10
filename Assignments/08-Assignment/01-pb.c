6692852fa68767e6c01ab3d6ff99f446bf7e473d
Menna <menna_nasser28@yahoo.com>
2020-11-10 18:19:10 +0200

    Making a file with a function that prints the elements of a given array

---
+++ Assignments/08-Assignment/01-pb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "D:\NTI\C_projects\Labs\07-Labs\STD_TYPES.h"
+
+#define SIZE 100
+
+void Read_Array (s32 arr[], s32 given_size);
+void ArrSum(s32 arr[], s32 given_size, s32 *sumPtr);
+
+int main(){
+	s32 ip_array[SIZE], array_size, array_sum=0;
+	
+	printf("Please enter the size of you array: ");
+	scanf("%d", &array_size);
+	
+	Read_Array(ip_array, array_size);
+	ArrSum (ip_array, array_size, &array_sum);
+	printf("The sum of all array elements = %d", array_sum);
+	
+	return 0;
+}
+
+void ArrSum(s32 arr[], s32 given_size, s32 *sumPtr){
+	s32 i;
+	for (i=0; i<given_size; i++){
+		*sumPtr = *sumPtr + arr[i];
+	}
+}
