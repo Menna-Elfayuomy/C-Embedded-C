8bac9751a00d2f30c8a38e2f04cb11d9cf6eb5ed
Menna <menna_nasser28@yahoo.com>
2020-11-13 17:43:48 +0200

    Adding pb2 program
    program files: 02-pb.c & 02-solu.exe
    program takes two numbers from the user.
    swap them with a function that uses pointers.
    the program makes the swapping operation using the XOR algorithm.

---
+++ Assignments/09-Assignment/02-pb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "D:\NTI\C_projects\Labs\07-Labs\STD_TYPES.h"
+#define SIZE 50
+
+void swapNumbers(s32 *num1Ptr, s32 *num2Ptr);
+
+int main(){
+	s32 num1, num2;
+	
+	printf("Please enter number 1 followed by number 2: \n");
+	scanf("%d%d", &num1, &num2);
+	swapNumbers(&num1, &num2);
+	printf("Numbers after swapping as following:\n Number 1 = %d \n Number 2 = %d\n", num1, num2);	
+	
+	return 0;
+}
+
+void swapNumbers(s32 *num1Ptr, s32 *num2Ptr){
+	*num1Ptr = *num1Ptr ^ *num2Ptr;
+	*num2Ptr = *num1Ptr ^ *num2Ptr;
+	*num1Ptr = *num1Ptr ^ *num2Ptr;
+}
