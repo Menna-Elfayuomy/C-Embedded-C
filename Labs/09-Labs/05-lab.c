ed08d96a1ebd78fcd155522f55372987b230ffaf
Menna <menna_nasser28@yahoo.com>
2020-11-13 16:25:57 +0200

    Adding lab5 program
    program files: 05-lab.c & 05-out.exe
    program takes input option for a know list and calls the equivalent function to the selected option number given by the user
    
    the program is A SIMPLE example on the following:
    - List of pointers to functions
    - Function that takes another function as arrgument. but the other function her e is not passed to it directly. instead the other function is an element in the a List of pointers
    - Pointer (to function) derefrecing

---
+++ Labs/09-Labs/05-lab.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "D:\NTI\C_projects\Labs\07-Labs\STD_TYPES.h"
+
+void func1(s32 y);
+void func2(s32 y);
+void func3(s32 y);
+void handleMenu (void (*funcPtr)(s32), s32 y);
+
+int main(){
+	//creating array of pointers to functions
+	void (*Arr[3])(s32 w)={func1, func2, func3};
+	
+	s32 x;
+	printf("Please enter a selection between 1 to 3: ");
+	scanf("%d", &x);
+	
+	handleMenu (Arr[x-1], x);
+	return 0;
+}
+
+//function that takes another function
+
+/*
+the function handleMenu is defined to take a function as input arrgmument 
+syntax is as following: 
+(return type) (name) (pointer to the other function, arrgument of the other function none from the decleration of its pointer)
+________________________________________________________
+the pointer to the other function 
+syntax is as following:
+(pointee function return type) (*name of the pointer) (type of arrguments of the pointee function)
+________________________________________________________
+inside the body of the function that takes another functiona as arrgument
+we commony call the other function using its address
+syntax of derefrencing the pointer as following:
+(*pointer name)(input arrguments of the other function)
+*/
+
+void handleMenu (void (*funcPtr)(s32), s32 y){
+	//derefrencing the pointer that points to a certain function
+	(*funcPtr)(y);
+}
+
+void func1(s32 y){
+	printf("You entered option %d, so function1 was called\n", y);
+}
+
+void func2(s32 y){
+	printf("You entered option %d, so function2 was called\n", y);
+}
+
+void func3(s32 y){
+	printf("You entered option %d, so function3 was called\n", y);
+}
