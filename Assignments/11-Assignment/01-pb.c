cf5707d71ccb2fbe8799beec6b486fe48fa15569
Menna <menna_nasser28@yahoo.com>
2020-11-15 23:42:18 +0200

    Modify the type of the roll number as the roll number is a unique number to identify the student in the database

--- Assignments/11-Assignment/01-pb.c
+++ Assignments/11-Assignment/01-pb.c
@@ -5,7 +5,7 @@
 
 typedef struct {
 	u8 name[SIZE];
-	u8 roll;
+	u32 roll;
 	s32 mark;
 }student_t;
 
@@ -14,14 +14,14 @@ int main(){
 	student_t student;
 	printf ("Please enter the name of the student: ");
 	scanf("%s", &student.name);
-	printf("Please enter the roll of the student either p for present or a for absent: ");
-	scanf(" %c", &student.roll);
+	printf("Please enter the student roll number: ");
+	scanf(" %d", &student.roll);
 	printf("Please enter the student mark: ");
 	scanf("%d", &student.mark);;
 	
 	printf("Student data are as following: \n");
 	printf("Student name: %s\n", student.name);
-	printf("Student roll is: %c\n", student.roll);
+	printf("Student roll number is: %d\n", student.roll);
 	printf("Student mark is: %d\n", student.mark);
 
 	return 0;
