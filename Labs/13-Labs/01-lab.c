1f2942681ba0002f1298ebc5cca51319d7ea9877
Menna <menna_nasser28@yahoo.com>
2020-11-18 02:46:24 +0200

    Modifying lab1 to work without the need for a resultant array. instead return the result in the same array passed from the user.

--- Labs/13-Labs/01-lab.c
+++ Labs/13-Labs/01-lab.c
@@ -5,11 +5,11 @@
 
 void Read_Array (s32 arr[], s32 given_size);
 void Print_Array (s32 arr[], s32 given_size);
-void Merge_Sorted_Parts (s32 arr[], s32 low1, s32 up1, s32 low2, s32 up2, s32 merged_parts[]);
+void Merge_Sorted_Parts (s32 arr[], s32 low1, s32 up1, s32 low2, s32 up2);
 
 int main(){
 	s32 Lw1, Lw2, Up1, Up2, size;
-	s32 Arr[SIZE], Result_Arr[SIZE];
+	s32 Arr[SIZE];
 	
 	printf("Please enter the array size: ");
 	scanf("%d", &size);
@@ -20,14 +20,15 @@ int main(){
 	printf("Please enter the low index and up undex for array 2: \n");
 	scanf("%d%d", &Lw2, &Up2);
 
-	Merge_Sorted_Parts(Arr, Lw1, Up1, Lw2, Up2, Result_Arr);
-	Print_Array (Result_Arr, size);
+	Merge_Sorted_Parts(Arr, Lw1, Up1, Lw2, Up2);
+	Print_Array (Arr, size);
 
 	return 0;
 }
 
-void Merge_Sorted_Parts (s32 arr[], s32 low1, s32 up1, s32 low2, s32 up2, s32 merged_parts[]){
-	s32 i=low1, j=low2, k=0;
+void Merge_Sorted_Parts (s32 arr[], s32 low1, s32 up1, s32 low2, s32 up2){
+	s32 i=low1, j=low2, k=low1, s=up2-low1+1;
+	s32 merged_parts[s];
 	while( (i<=up1) && (j<=up2) ){
 		if (arr[i]<arr[j]){
 			merged_parts[k]=arr[i];
@@ -54,4 +55,7 @@ void Merge_Sorted_Parts (s32 arr[], s32 low1, s32 up1, s32 low2, s32 up2, s32 me
 		k++;
 	}
 	
+	for (i=low1; i<up2; i++){
+		arr[i]=merged_parts[i];
+	}
 }
