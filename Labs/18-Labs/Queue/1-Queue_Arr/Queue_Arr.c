276e35970067c9c4688dc582b2ab43d7086ef0ff
Menna <menna_nasser28@yahoo.com>
2020-11-24 14:35:35 +0200

    Adding the library of queue using arrays

---
+++ Labs/18-Labs/Queue/1-Queue_Arr/Queue_Arr.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../../../MyLibraries/STD_TYPES.h"
+#include "Queue_Arr.h"
+
+#define Max		50
+
+//global variables and are set to be internal linkage with the word static
+static s32 Arr[Max];
+static s32 Front, Rear;
+
+void Queue_Init(void)
+{
+	Front = -1;
+	Rear = -1;
+}
+
+u8 Queue_IsEmpty(void)
+{
+	u8 ErrorState = OK;
+	if ((Front==-1) || (Front==Rear+1))
+	{
+		printf("Queue is empty\n");
+	}
+	else
+		ErrorState = NOK;
+	return ErrorState;
+}
+
+u8 Queue_IsFull(void)
+{
+	u8 ErrorState = OK;
+	if (Rear==Max-1)
+	{
+		printf ("Queue is full\n\n");
+	}		
+	else
+		ErrorState=NOK;
+	return ErrorState;
+}
+
+u32 Queue_GetSize(void)
+{
+	//check if empty\n
+	return Rear-Front+1;
+}
+
+u8 Queue_Insert(s32 Data)
+{
+	u8 ErrorState = OK;
+	if (Queue_IsFull())
+	{
+		printf("Queue is full, inserting will result in overflow\n\n");
+		ErrorState = NOK;
+	}
+	else 
+	{
+		// check if it's the 1st time insertion
+		if (Front == -1)
+		{
+			Front=0;
+		}
+		Rear++;
+		Arr[Rear]=Data;
+		printf("Insertion in is done\n\n");
+	}
+	return ErrorState;
+}
+
+u8 Queue_Delete(s32* DataPtr)
+{
+	u8 ErrorState = OK;
+	if (DataPtr == NULL)
+	{
+		ErrorState = NOK;
+	}
+	else if (Queue_IsEmpty())
+	{
+		printf("Queue is already empty, deletion will result in underflow\n\n");
+		ErrorState=NOK;
+	}
+	else
+	{
+		*DataPtr = Arr[Front];
+		Front++;
+	}
+	return ErrorState;
+}
+
+u8 Queue_Peek(s32* DataPtr)
+{
+	u8 ErrorState = OK;
+	if (DataPtr == NULL)
+	{
+		ErrorState = NOK;
+	}
+	else if (Queue_IsEmpty())
+	{
+		printf("Queue is already empty\n\n");
+		ErrorState=NOK;
+	}
+	else
+	{
+		*DataPtr = Arr[Front];
+	}
+	return ErrorState;
+}
+
+void Queue_Display(void)
+{
+	u32 Counter;
+	if (Queue_IsEmpty())
+	{
+		//do nothing
+	}
+	else
+	{
+		for (Counter=Front; Counter<=Rear; Counter++)
+		{
+			printf("%d\t", Arr[Counter]);
+		}
+		printf("\n\n");
+	}
+}
